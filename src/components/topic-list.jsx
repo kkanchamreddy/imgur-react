7a9bbbe953b8863b09fe372b7c361b73cb751009
kkanchamreddy <kiran.kanchamreddy@gmail.com>
2016-01-27 00:14:51 -0800

    Add topic routes and react router history

--- src/components/topic-list.jsx
+++ src/components/topic-list.jsx
@@ -1,7 +1,15 @@
 var React = require('react');
 var TopicStore = require('../stores/topic-store');
+var Reflux = require('reflux');
+var Actions = require('../actions');
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
 
 module.exports = React.createClass({
+  mixins: [
+    Reflux.listenTo(TopicStore, 'onChange')
+  ],
+
   getInitialState: function() {
     return {
       topics: []
@@ -9,27 +17,27 @@ module.exports = React.createClass({
   },
 
   componentWillMount: function() {
-    TopicStore.getTopics()
-      .then(function(){
-        this.setState({
-          topics: TopicStore.topics
-        })
-      }.bind(this));
+    Actions.getTopics();
   },
 
   render: function(){
     return <div className="list-group">
-      Topic List
       {this.renderTopics()}
     </div>
   },
 
   renderTopics: function() {
     return this.state.topics.map(function(topic){
-      console.log(topic);
-      return <li>
-        {topic.name}
-      </li>
-    })
+      return <Link to={"topics/" + topic.id} className="list-group-item" key={topic.id}>
+        <h4>{topic.name}</h4>
+        <p> {topic.description}</p>
+      </Link>
+    });
+  },
+
+  onChange: function(event, topics) {
+    this.setState({
+      topics: topics
+    });
   }
 })
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,14 +1,16 @@
 var React = require('react');
 var ReactRouter = require('react-router');
+var History = require('react-router').browserHistory;
 var Router = ReactRouter.Router;
 var Route = ReactRouter.Route;
 
 var Main = require('./components/main');
+var Topic = require('./components/topic');
 
 module.exports = (
-  <Router>
+  <Router history = {History}>
     <Route path="/" component = {Main}>
-
+      <Route path="topics/:id" component ={Topic} />
     </Route>
   </Router>
 );
