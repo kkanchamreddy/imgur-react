d979cdb1a0bae9d1c746752545684bfe34d4f7ac
kkanchamreddy <kiran.kanchamreddy@gmail.com>
2016-01-28 08:22:11 -0800

    Play animated videos

--- src/components/image-preview.jsx
+++ src/components/image-preview.jsx
@@ -1,13 +1,44 @@
 var React = require('react');
 
 module.exports = React.createClass({
+  getInitialState: function(){
+    return {
+      hovering: false
+    }
+  },
+
   render: function() {
-    return <div className="image-preview">
-        {this.image()}
+    return <div
+        className = "image-preview"
+        onMouseEnter = {this.handleMouseEnter}
+        onMouseLeave = {this.handleMouseLeave}>
+
+        {this.props.animated && this.state.hovering ? this.video(): this.image()}
     </div>
   },
+
+  video: function() {
+    return <div>
+      <video preload = "auto" autoPlay = "autoPlay" loop = "loop" webkit-playsinline>
+        <source src= {this.props.mp4} type ="video/mp4"> </source>
+      </video>
+    </div>
+  },
+
   image: function() {
     var link = 'http://i.imgur.com/' + this.props.id + 'h.jpg';
     return <img src = {link}/>
+  },
+
+  handleMouseEnter: function() {
+    this.setState({
+      hovering: true
+    });
+  },
+
+  handleMouseLeave: function() {
+    this.setState({
+      hovering: false
+    });
   }
 })
