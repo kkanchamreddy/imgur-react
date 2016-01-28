48c66ab78868e9fd5fa840980dc761bd3aadcc5c
kkanchamreddy <kiran.kanchamreddy@gmail.com>
2016-01-27 19:11:15 -0800

    setup ImageStore

--- src/components/topic.jsx
+++ src/components/topic.jsx
@@ -1,7 +1,33 @@
 var React = require('react');
+var ImageStore = require('../stores/image-store');
+var Reflux = require('reflux');
+var Actions = require('../actions');
 
 module.exports = React.createClass({
-  render: function(){
-    return <div> {this.props.params.id}</div>
+  mixins: [
+    Reflux.listenTo(ImageStore, 'onChange')
+  ],
+
+  getInitialState: function() {
+    return {
+      images: []
+    }
+  },
+
+  componentWillMount: function() {
+    Actions.getImages(this.props.params.id);
+  },
+
+  render: function() {
+    return this.state.images.map(function(image){
+      return <img src={image}/>
+
+    })
+  },
+
+  onChange: function(event, images) {
+    this.setState({
+      images: images
+    });
   }
 })
